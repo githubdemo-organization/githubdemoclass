bd3e9588cdef22b8b655cfd55697cb8f20b47b67
TimSocialCDE <tim.thompson.socialCDE@gmail.com>
2015-09-22 19:49:33 +0200

    Commit Tim

--- src/githubdemoclass/ProvaDue.java
+++ src/githubdemoclass/ProvaDue.java
@@ -5,6 +5,7 @@ public class ProvaDue {
 	public static void main(String[] args) {
 		System.out.println("Commit Alice");
 		System.out.println("Commit Bob");
+		System.out.println("Commit Tim");
 	}
 
 }
