9f28abc6ac809b943664eae0dfbc66fdd224677e
Bob Green <bob.green.socialCDE@gmail.com>
2015-10-15 17:06:18 +0200

    Create ProvaTre.java

---
+++ src/githubdemoclass/ProvaTre.java
@@ -0,0 +1,11 @@
+package githubdemoclass;
+
+public class ProvaDue {
+
+	public static void main(String[] args) {
+		System.out.println("Commit Alice");
+		System.out.println("Commit Bob");
+		System.out.println("Commit Tim");
+	}
+
+}
